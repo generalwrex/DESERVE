f3022c1178ee8d0bc37940ae0cf57de3c4986f0c
Commander-Scott <ScottClayton.erg@gmail.com>
2014-10-30 15:32:21 -0400

    ChatMessage class added.

---
+++ DESERVE.Common/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DESERVE.Common
+{
+	[DataContract]
+	class ChatMessage
+	{
+		[DataMember]
+		public String Message { get; set; }
+		[DataMember]
+		public ulong SteamId { get; set; }
+		[DataMember]
+		public String Name { get; set; }
+	}
+}
