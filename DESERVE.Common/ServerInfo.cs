a04648daf4c801da78d4e888827cf233916c2727
Commander-Scott <ScottClayton.erg@gmail.com>
2014-10-30 20:32:13 -0400

    And one more

--- DESERVE.Common/ServerInfo.cs
+++ DESERVE.Common/ServerInfo.cs
@@ -6,22 +6,15 @@ using System.Collections.ObjectModel;
 namespace DESERVE.Common
 {
 	[DataContract]
-	public class ServerInfo
+	public class ServerInfo : ServerInfoPartial
 	{
 		#region Properties
 		[DataMember]
 		public virtual String Name { get; set; }
 		[DataMember]
-		public virtual Boolean IsRunning { get; set; }
-		[DataMember]
 		public virtual ObservableCollection<Player> CurrentPlayers { get; set; }
 		[DataMember]
-		public virtual TimeSpan Uptime { get; set; }
-		[DataMember]
-		public virtual DateTime LastSave { get; set; }
-		[DataMember]
 		public virtual ObservableCollection<ChatMessage> ChatMessages { get; set; }
-
 		#endregion
 
 		public ServerInfo(String name, Boolean isRunning, ObservableCollection<Player> currentPlayers, TimeSpan uptime, DateTime lastSave, ObservableCollection<ChatMessage> chatMessages)
@@ -37,42 +30,16 @@ namespace DESERVE.Common
 		public ServerInfo() { }
 	}
 
-	public delegate void ServerStateEvent();
-
-	[ServiceContract(SessionMode = SessionMode.Required,
-	CallbackContract = typeof(IWCFClient))]
-	public interface IWCFService
+	[DataContract]
+	public class ServerInfoPartial
 	{
-		/// <summary>
-		/// Starts the process of stopping the server.
-		/// </summary>
-		[OperationContract(IsOneWay = true)]
-		void Stop();
-
-		/// <summary>
-		/// Used to make the server save the game.
-		/// </summary>
-		[OperationContract(IsOneWay = true)]
-		void Save();
-
-		/// <summary>
-		/// Used to request server information from the server.
-		/// </summary>
-		[OperationContract(IsOneWay = true)]
-		void RequestUpdate();
-
-		[OperationContract(IsOneWay = true)]
-		void SendChatMessage(ChatMessage message);
-
-	}
-
-	public interface IWCFClient
-	{
-		/// <summary>
-		///
-		/// </summary>
-		/// <param name="serverInfo"></param>
-		[OperationContract(IsOneWay = true)]
-		void ServerUpdate(ServerInfo serverInfo);
+		#region Properties
+		[DataMember]
+		public virtual Boolean IsRunning { get; set; }
+		[DataMember]
+		public virtual TimeSpan Uptime { get; set; }
+		[DataMember]
+		public virtual DateTime LastSave { get; set; }
+		#endregion
 	}
 }
